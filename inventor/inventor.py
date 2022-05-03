7d5a599eb75b4dabd63c50749185b18f1003ed06
Gabriel Han <gabrielhan104@gmail.com>
2022-05-03 14:59:54 -0700

    hi

--- inventor/inventor.py
+++ inventor/inventor.py
@@ -8,23 +8,16 @@ class Vision:
 
     def __init__(self):
         self.timer = jevois.Timer("processing timer", 100, jevois.LOG_INFO)
-        # self.hsv_low = np.array([86, 40, 200])
-        # self.hsv_high = np.array([90,230,255])
+        # self.hsv_low = np.array([70, 100, 100])
+        # self.hsv_high = np.array([100,255,255])
         self.hsv_low = np.array([70, 40, 130])
         self.hsv_high = np.array([90,255,255])
-        # self.hsv_low = np.array([75, 50, 120])
-        # self.hsv_high = np.array([90,255,255])
-        # self.hsv_low = np.array([85, 40, 200])
-        # self.hsv_high = np.array([90,230,255])
-        # self.hsv_low = np.array([0, 100, 100])
-        # self.hsv_high = np.array([150,200,255])
 
     def processInformation(self, inimg):
         inimg = inimg.getCvBGR()
         frame = cv2.cvtColor(inimg,cv2.COLOR_BGR2HSV)
         filtered_frame = cv2.inRange(frame,self.hsv_low, self.hsv_high)
-        frame_width = filtered_frame.shape[1]
-        frame_height = filtered_frame.shape[0]
+        frame_height, frame_width = filtered_frame.shape
         contours, _ = cv2.findContours(filtered_frame, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
         filtered_contours = []
         avx,avy = 0,0
@@ -46,15 +39,10 @@ class Vision:
             radius = 10
             color = (255, 0, 0)
             thickness = 2
-            
             filtered_frame = cv2.circle(filtered_frame, center_coordinates, radius, color, thickness)
         else:
             avx,avy = 320/2,240/2
-
-        # cv2.putText(filtered_frame, "JeVois RapidReact", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))         
-        # cv2.putText(filtered_frame, (3, frame_height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
         sendData = str((avx-frame_width/2)*75/frame_width)+";"+str(avy)
-        # sendData = "10.1;3200"
         jevois.sendSerial(sendData)
         return filtered_frame
 
--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-tk.Button(root, text="HI", padx=40,pady=10,bg="blue").pack()
-root.mainloop()
